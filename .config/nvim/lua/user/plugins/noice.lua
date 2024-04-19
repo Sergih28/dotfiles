470a1899572423f0730e5135ba20c7db98408b07
Sergi <Sergih28@users.noreply.github.com>
2024-04-19 20:45:00 +0100

    chore(noice): disable plugin
    It is an interesting plugin, but notifications become very annoying.

--- .config/nvim/lua/user/plugins/noice.lua
+++ .config/nvim/lua/user/plugins/noice.lua
@@ -1,23 +1,23 @@
-return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    { "MunifTanjim/nui.nvim" },
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    {
-      "rcarriga/nvim-notify",
-      config = function ()
-        require("notify").setup({
-          -- Needed for 100% transparency
-          background_colour = "#000000"
-        })
-      end
-    }
-  }
-}
+return {}
+  -- "folke/noice.nvim",
+  -- event = "VeryLazy",
+  -- opts = {
+  --   -- add any options here
+  -- },
+  -- dependencies = {
+  --   -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --   { "MunifTanjim/nui.nvim" },
+  --   -- OPTIONAL:
+  --   --   `nvim-notify` is only needed, if you want to use the notification view.
+  --   --   If not available, we use `mini` as the fallback
+  --   {
+  --     "rcarriga/nvim-notify",
+  --     config = function ()
+  --       require("notify").setup({
+  --         -- Needed for 100% transparency
+  --         background_colour = "#000000"
+  --       })
+  --     end
+  --   }
+  -- }
+-- }
