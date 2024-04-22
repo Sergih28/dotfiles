b452711c4cb2fe4667e6e865be5dfa49aa722537
Sergi <Sergih28@users.noreply.github.com>
2024-04-22 07:37:12 +0100

    chore(mini): update mini pairs config

--- .config/nvim/lua/user/plugins/lualine.lua
+++ .config/nvim/lua/user/plugins/lualine.lua
@@ -1,76 +1,105 @@
 local function fileIcon()
-  return ''
+  return ""
 end
 
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = function()
-    local custom_nightfly = require'lualine.themes.nightfly'
+    local custom_nightfly = require("lualine.themes.nightfly")
 
     -- Change the background to transparent
-    custom_nightfly.normal.b.bg = 'NONE'
-    custom_nightfly.normal.c.bg = 'NONE'
-    custom_nightfly.insert.b.bg = 'NONE'
-    custom_nightfly.visual.b.bg = 'NONE'
-    custom_nightfly.replace.b.bg = 'NONE'
-    custom_nightfly.command.b.bg = 'NONE'
-    custom_nightfly.inactive.b.bg = 'NONE'
+    custom_nightfly.normal.b.bg = "NONE"
+    custom_nightfly.normal.c.bg = "NONE"
+    custom_nightfly.insert.b.bg = "NONE"
+    custom_nightfly.visual.b.bg = "NONE"
+    custom_nightfly.replace.b.bg = "NONE"
+    custom_nightfly.command.b.bg = "NONE"
+    custom_nightfly.inactive.b.bg = "NONE"
 
     return {
       options = {
         theme = custom_nightfly,
-        component_separators = { left = '|', right = '|'},
-        section_separators = { left = '', right = ''},
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
       },
       sections = {
-        lualine_a = {'mode'},
-        lualine_b = {require('auto-session.lib').current_session_name, 'branch', 'diff', 'diagnostics'},
+        lualine_a = { "mode" },
+        -- lualine_b = {require('auto-session.lib').current_session_name, 'branch', 'diff', 'diagnostics'},
+        lualine_b = { "diff", "diagnostics" },
         lualine_c = {
           {
-            'filetype',
+            "filetype",
             icon_only = true,
-            separator = '',
-            icon = {align = 'left'},
+            separator = "",
+            icon = { align = "left" },
             padding = { left = 1, right = 0 },
           },
           {
-            'filename',
+            "filename",
             file_status = true,
             path = 1,
             symbols = {
-              modified = '*'
+              modified = "*",
             },
-            separator = '>',
-            color = {fg='SkyBlue1'},
+            separator = ">",
+            color = { fg = "SkyBlue1" },
           },
           {
             fileIcon,
-            separator = '',
+            separator = "",
             padding = { left = 1, right = 0 },
-            color = {fg='gold'},
+            color = { fg = "gold" },
           },
           {
-            'filesize',
-            color = {fg='burlywood1'},
+            "filesize",
+            color = { fg = "burlywood1" },
           },
         },
         lualine_x = {
           {
-            'encoding',
-            color = {fg='aquamarine1'},
+            "encoding",
+            color = { fg = "aquamarine1" },
+            padding = { left = 0, right = 0 },
+            separator = "",
           },
           {
-            'fileformat',
-            color = {fg='gold'},
+            "fileformat",
+            color = { fg = "gold" },
+            padding = { left = 1, right = 0 },
           },
-          {
-            'filetype',
-            color = {fg='SkyBlue1'},
-          }
+          -- {
+          --   'filetype',
+          --   color = {fg='SkyBlue1'},
+          -- }
         },
-        lualine_y = {'progress', 'searchcount', 'selectioncount', 'location'},
-        lualine_z = {'os.date(" %I:%M %p")'}
+        lualine_y = {
+          {
+            "progress",
+            padding = { left = 1, right = 0 },
+            color = { fg = "SkyBlue1" },
+            separator = "",
+          },
+          {
+            "selectioncount",
+            padding = { left = 1, right = 0 },
+            color = { fg = "aquamarine1" },
+            separator = "",
+          },
+          {
+            "searchcount",
+            padding = { left = 1, right = 0 },
+            color = { fg = "burlywood1" },
+            separator = "",
+          },
+          {
+            "location",
+            padding = { left = 0, right = 1 },
+            separator = "",
+          },
+        },
+        -- lualine_z = {'os.date(" %I:%M %p")'}
+        lualine_z = {},
       },
     }
   end,
