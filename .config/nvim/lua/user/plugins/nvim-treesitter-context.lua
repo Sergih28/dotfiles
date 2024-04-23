d10409bb68eeba7281eb4c07e34ef6081ab12b5b
Sergi <Sergih28@users.noreply.github.com>
2024-04-23 18:29:22 +0100

    chore(nvim-treesitter-context): add line threshold to 1
    This way when we have a function with lots of arguments
    it just appears as 1 line folded and does not include
    all the other lines, which eats up a lot of space

--- .config/nvim/lua/user/plugins/nvim-treesitter-context.lua
+++ .config/nvim/lua/user/plugins/nvim-treesitter-context.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter-context",
   config = function()
     require'treesitter-context'.setup {
+      multiline_threshold = 1,
       enable = true,
     }
   end;
