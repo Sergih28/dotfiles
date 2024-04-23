563ac4e59445900f1d039358dc717452b80f3263
Sergi <Sergih28@users.noreply.github.com>
2024-04-23 21:09:33 +0100

    chore(symbol-usage): exclude declaration line from reference

--- .config/nvim/lua/user/plugins/symbol-usage.lua
+++ .config/nvim/lua/user/plugins/symbol-usage.lua
@@ -2,6 +2,10 @@ return {
   "Wansmer/symbol-usage.nvim",
   event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
   config = function()
-    require("symbol-usage").setup()
+    require("symbol-usage").setup({
+      references = { enabled = true, include_declaration = false },
+      definition = { enabled = false },
+      implementation = { enabled = false },
+    })
   end,
 }
