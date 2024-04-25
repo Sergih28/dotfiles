384763af9fb7264a122566400dd425f28496f589
Sergi <Sergih28@users.noreply.github.com>
2024-04-25 20:50:32 +0100

    chore(harpoon): update open harpoon keybind

--- .config/nvim/lua/user/plugins/harpoon.lua
+++ .config/nvim/lua/user/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():add()
     end)
-    vim.keymap.set("n", "<leader><S-e>", function()
+    vim.keymap.set("n", "<leader><h>", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
     vim.keymap.set("n", "<C-h>", function()
