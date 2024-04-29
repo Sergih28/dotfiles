5cbb0790a2c8610be6c581b3fdb8307fd45dfb96
Sergi <Sergih28@users.noreply.github.com>
2024-04-30 00:29:07 +0100

    chore: delte treesitter things (will be added in a later comit)

--- .config/nvim/lua/user/plugins/nvim-treesitter-context.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter-context",
-  config = function()
-    require'treesitter-context'.setup {
-      multiline_threshold = 1,
-      enable = true,
-    }
-  end;
-}
