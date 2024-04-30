88f6202a46f4858e8ca8235ba06cc70cdf1c0a29
Sergi <Sergih28@users.noreply.github.com>
2024-04-30 17:26:33 +0100

    chore(hardtime): remove hints and notifications
    I just want it to check the stats from time to time, but so many
    notifications are annoying for a usual workflow of moving a lot with j/k

--- .config/nvim/lua/user/plugins/hardtime.lua
+++ .config/nvim/lua/user/plugins/hardtime.lua
@@ -5,5 +5,7 @@ return {
     cmdheight = 2,
     restriction_mode = "hint", -- "block" is the default mode, but it blocks movement when it's not efficient, which I can find annoying
     disable_mouse = false, -- I think it's annoying if you just want to click or select something to copy with your mouse at some point if this is enabled (by default)
+    hint = false, -- So i don't see it popping up every time. I think I just want this to see stats from time to time, but don't appear on screen.
+    notification = false, -- Same as above
   },
 }
