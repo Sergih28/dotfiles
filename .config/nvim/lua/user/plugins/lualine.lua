3d1ab36cffaa65247385b19365d5dd498bbc5f38
Sergi <Sergih28@users.noreply.github.com>
2024-05-01 23:47:59 +0100

    chore: update nvim theme to catppuccin
    Commented some old themes stuff for now.
    Not 100% sure of using this one yet, but I think with minimal tweaks
    will be better for my eyes, as it doesn't have that many contrasting
    colors. So as long as I can easily distinguish them, is good enough.

--- .config/nvim/lua/user/plugins/lualine.lua
+++ .config/nvim/lua/user/plugins/lualine.lua
@@ -6,27 +6,39 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = function()
-    local custom_nightfly = require("lualine.themes.nightfly")
-
-    -- Change the background to transparent
-    custom_nightfly.normal.b.bg = "NONE"
-    custom_nightfly.normal.c.bg = "NONE"
-    custom_nightfly.insert.b.bg = "NONE"
-    custom_nightfly.visual.b.bg = "NONE"
-    custom_nightfly.replace.b.bg = "NONE"
-    custom_nightfly.command.b.bg = "NONE"
-    custom_nightfly.inactive.b.bg = "NONE"
+    -- local custom_nightfly = require("lualine.themes.nightfly")
+    --
+    -- -- Change the background to transparent
+    -- custom_nightfly.normal.b.bg = "NONE"
+    -- custom_nightfly.normal.c.bg = "NONE"
+    -- custom_nightfly.insert.b.bg = "NONE"
+    -- custom_nightfly.visual.b.bg = "NONE"
+    -- custom_nightfly.replace.b.bg = "NONE"
+    -- custom_nightfly.command.b.bg = "NONE"
+    -- custom_nightfly.inactive.b.bg = "NONE"
 
     return {
       options = {
-        theme = custom_nightfly,
+        theme = "catppuccin", --custom_nightfly,
         component_separators = { left = "|", right = "|" },
         section_separators = { left = "", right = "" },
       },
       sections = {
         lualine_a = { "mode" },
-        -- lualine_b = {require('auto-session.lib').current_session_name, 'branch', 'diff', 'diagnostics'},
-        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_b = {
+          {
+            "branch",
+            color = { bg = "NONE" },
+          },
+          {
+            "diff",
+            color = { bg = "NONE" },
+          },
+          {
+            "diagnostics",
+            color = { bg = "NONE" },
+          },
+        },
         lualine_c = {
           {
             "filetype",
@@ -34,6 +46,7 @@ return {
             separator = "",
             icon = { align = "left" },
             padding = { left = 1, right = 0 },
+            color = { bg = "NONE" },
           },
           {
             "filename",
@@ -43,58 +56,59 @@ return {
               modified = "*",
             },
             separator = ">",
-            color = { fg = "SkyBlue1" },
+            color = { bg = "NONE", fg = "#87B0F9" },
           },
           {
             fileIcon,
             separator = "",
             padding = { left = 1, right = 0 },
-            color = { fg = "gold" },
+            color = { bg = "NONE", fg = "#FAB387" },
           },
           {
             "filesize",
-            color = { fg = "burlywood1" },
+            color = { bg = "NONE", fg = "#A6E3A1" },
           },
         },
         lualine_x = {
           {
             "encoding",
-            color = { fg = "aquamarine1" },
+            color = { bg = "NONE", fg = "#CBA6F7" },
             padding = { left = 0, right = 0 },
             separator = "",
           },
           {
             "fileformat",
-            color = { fg = "gold" },
+            color = { bg = "NONE", fg = "#FAB387" },
             padding = { left = 1, right = 0 },
           },
           -- {
           --   'filetype',
-          --   color = {fg='SkyBlue1'},
+          --   color = {fg='#87B0F9'},
           -- }
         },
         lualine_y = {
           {
             "progress",
             padding = { left = 1, right = 0 },
-            color = { fg = "SkyBlue1" },
+            color = { bg = "NONE", fg = "#87B0F9" },
             separator = "",
           },
           {
             "selectioncount",
             padding = { left = 1, right = 0 },
-            color = { fg = "aquamarine1" },
+            color = { bg = "NONE", fg = "#CBA6F7" },
             separator = "",
           },
           {
             "searchcount",
             padding = { left = 1, right = 0 },
-            color = { fg = "burlywood1" },
+            color = { bg = "NONE", fg = "#A6E3A1" },
             separator = "",
           },
           {
             "location",
             padding = { left = 0, right = 1 },
+            color = { bg = "NONE" },
             separator = "",
           },
         },
