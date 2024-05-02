6faf7a98960ad1602b63eeb84030ad712a6cabae
Sergi <Sergih28@users.noreply.github.com>
2024-05-02 23:08:57 +0100

    chore(barbecue): add plugin and setup to not show on some filetype like terminal or alpha

---
+++ .config/nvim/lua/user/plugins/barbecue.lua
@@ -0,0 +1,17 @@
+return {
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  version = "*",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons", -- optional dependency
+  },
+  opts = {
+    exclude_filetypes = {
+      "alpha",
+      "netrw",
+      "toggleterm",
+      "NvimTree",
+    }
+  },
+}
--- .config/nvim/lua/user/plugins/toggleterm.lua
+++ .config/nvim/lua/user/plugins/toggleterm.lua
@@ -1,8 +1,15 @@
 return {
-  'akinsho/toggleterm.nvim',
+  "akinsho/toggleterm.nvim",
   version = "*",
-  config = function ()
-    require("toggleterm").setup()
+  config = function()
+    require("toggleterm").setup({
+      -- this is needed for barbecue.nvim to not show at first when opening the terminal
+      on_open = function(term)
+        vim.defer_fn(function()
+          vim.wo[term.window].winbar = ""
+        end, 0)
+      end,
+    })
   end,
   opts = {},
 }
