2fa8ef2e680c892c391059463da138ea12d8c092
Sergi <Sergih28@users.noreply.github.com>
2024-05-08 17:48:31 +0100

    feat(recall): create a shada file for each project, so that bookmarks are isolated

--- .config/nvim/lua/user/plugins/recall.lua
+++ .config/nvim/lua/user/plugins/recall.lua
@@ -5,5 +5,13 @@ return {
     local recall = require("recall")
 
     recall.setup({})
+
+    -- Get the name of the current working directory.
+    local function cwd_name()
+      return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+    end
+    -- This will create a shada file for each project
+    -- to keep the bookmarks separate
+    vim.opt.shadafile = os.getenv("HOME") .. "/shadas/" .. cwd_name() .. ".shada"
   end,
 }
