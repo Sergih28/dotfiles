7a17b6c8b987e9788afb5ad11296f773054638d2
Sergi <Sergih28@users.noreply.github.com>
2024-05-08 17:44:29 +0100

    refactor(recall): move keybinds to keymaps file

--- .config/nvim/lua/user/plugins/recall.lua
+++ .config/nvim/lua/user/plugins/recall.lua
@@ -5,11 +5,5 @@ return {
     local recall = require("recall")
 
     recall.setup({})
-
-    vim.keymap.set("n", "<leader>bt", recall.toggle, { noremap = true, silent = true, desc = "[B]ookmark [T]oggle" })
-    vim.keymap.set("n", "]b", recall.goto_next, { noremap = true, silent = true, desc = "Next [B]ookmark" })
-    vim.keymap.set("n", "[b", recall.goto_prev, { noremap = true, silent = true, desc = "Previous [B]ookmark" })
-    vim.keymap.set("n", "<leader>bd", recall.clear, { noremap = true, silent = true, desc = "[B]ookmarks [D]elete" })
-    vim.keymap.set("n", "<leader>bl", ":Telescope recall<CR>", { noremap = true, silent = true, desc = "Show [B]ookmarks [L]ist" })
   end,
 }
