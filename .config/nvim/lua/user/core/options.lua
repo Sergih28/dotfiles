2a55db0b8351ecf0d78dd1503bcbc81280afb7d6
Sergi <Sergih28@users.noreply.github.com>
2024-11-23 11:19:37 +0000

    fix(nvim): remove ttymouse option, it breaks the config

--- .config/nvim/lua/user/core/options.lua
+++ .config/nvim/lua/user/core/options.lua
@@ -69,5 +69,5 @@ end
 -- Set ColorColumn to match the normal background color
 vim.cmd("highlight ColorColumn guibg=NONE ctermbg=NONE")
 
-vim.o.ttymouse = "xterm2"
+--vim.o.ttymouse = "xterm2"
 vim.o.mouse = "nvia"
