7912313cef89aab183cd864ebd9fe746b3a58f14
Sergi <Sergih28@users.noreply.github.com>
2024-12-02 21:14:11 +0000

    feat(nvim): add keymap to search the current selection

--- .config/nvim/lua/user/core/keymaps.lua
+++ .config/nvim/lua/user/core/keymaps.lua
@@ -40,6 +40,7 @@ map("v", "p", '"_dP', "Replace paste so it does not copy what you replaced")
 
 -- Inside a file
 map("n", "*", function() vim.cmd("normal! *") vim.cmd("normal! N") end, "Remap * to stay in the first word")
+map("v", "/", "\"fy/\\V<C-R>f<CR>N", "Search selection, staying on the same word")
 map("n", "<leader><S-a>", "gg<S-v>G$", "Select all")
 map("n", "<C-f>", "<C-f>zz", "Center when moving one page down")
 map("n", "<C-b>", "<C-b>zz", "Center when moving one page up")
