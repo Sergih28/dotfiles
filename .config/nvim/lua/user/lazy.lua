b2a0d01877a4afe398b7af12f688f0156ab042be
Sergi <Sergih28@users.noreply.github.com>
2024-12-06 17:12:39 +0000

    refactor(nvim): remove some lazy comments

--- .config/nvim/lua/user/lazy.lua
+++ .config/nvim/lua/user/lazy.lua
@@ -40,9 +40,6 @@ require("lazy").setup({
   ui = {
     border = "single",
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "habamax" } },
   -- disable automatically check for plugin updates
   checker = { enabled = false },
 })
